a7bd55775a00812759ab38cf4d4a7511133f67f9
Bryan Mytko <bryanmytko@gmail.com>
2021-04-09 14:40:13 -0400

    Add DMX override

---
+++ src/commands/playlist.js
@@ -0,0 +1,17 @@
+module.exports = async (params) => {
+  // const { queue, message } = params;
+  //
+  // 1. !playlist <playlist name> <command> <url>
+  //
+  // <play>
+  // 1. lookup playlist by playlist name
+  // 2. add each song to queue
+  //
+  // <add>
+  // 1. lookup playlist by playlist name
+  // 2. add song or reject if already there
+  //
+  // <remove>
+  // 1. lookup playlist by playlist name
+  // 2. remove song or reject if missing
+};
--- src/commands/song.js
+++ src/commands/song.js
@@ -73,7 +73,34 @@ module.exports = async (params) => {
   let results;
 
   try {
-    results = await youtube.searchVideos(cleanParams, 1);
+    /* DMX */
+    const dmxSongs = [
+      'DMX where the hood at',
+      'DMX what these bitches want',
+      'DMX ruff ryders anthem',
+      'DMX party up',
+      'DMX we right here',
+      'DMX the rain',
+      'DMX slippin',
+      'DMX here we go again',
+      'DMX stop being greedy',
+      'DMX grand finale',
+      'DMX its all good',
+      'DMX get at me dog',
+      'DMX whats my name',
+      'DMX x gon give it to ya',
+      'DMX how its goin down',
+      'DMX one more road to cross',
+      'DMX get it on the floor',
+      'DMX damien',
+      'DMX who we be',
+      'DMX no love for me',
+      'DMX blackout',
+    ];
+
+    const dmxSong = dmxSongs[Math.floor(Math.random() * dmxSongs.length)];
+
+    results = await youtube.searchVideos(`${dmxSong} lyrics`, 1);
   } catch (e) {
     logger.error(e);
     return message.channel.send(MSG_YOUTUBE_ERROR);
