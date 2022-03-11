ba819a3025b23c167a885516f5d39fd43f2460e1
Bryan Mytko <bryanmytko@gmail.com>
2022-03-11 06:44:31 -0500

    WIP set up reconnect command

--- src/commands/reconnect.js
+++ src/commands/reconnect.js
@@ -1,9 +1,10 @@
 const logger = require('pino')({ prettyPrint: true });
 
-const playSong = require('../util/play_song');
+const { playSong } = require('../util/player');
 
 module.exports = async (params) => {
   // signature:
   // playlist, message, queue, queueConstruct.songs[0], message.guild
-  playSong(params);
+  // playSong(params);
+  logger.info('This is where i join the last channel and start playing the playlist.')
 };
