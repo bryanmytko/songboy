23dd0cc4c7daa81cf02273f27e13f39c0bbab90e
Bryan Mytko <bryanmytko@gmail.com>
2022-03-11 06:43:41 -0500

    export reconnect

--- src/commands/index.js
+++ src/commands/index.js
@@ -2,3 +2,4 @@ exports.song = require('./song');
 exports.stop = require('./stop');
 exports.queue = require('./queue');
 exports.skip = require('./skip');
+exports.reconnect = require('./reconnect');
