86c25b180121f1952006ea6ef116a66817f1ab39
Bryan Mytko <bryanmytko@gmail.com>
2022-03-12 05:59:42 -0500

    Flesh out object for reconnect

--- src/commands/reconnect.js
+++ src/commands/reconnect.js
@@ -1,10 +1,23 @@
 const logger = require('pino')({ prettyPrint: true });
+const { DEFAULT_VOLUME } = require('../util/constants');
 
 const { playSong } = require('../util/player');
 
 module.exports = async (params) => {
-  // signature:
-  // playlist, message, queue, queueConstruct.songs[0], message.guild
-  // playSong(params);
-  logger.info('This is where i join the last channel and start playing the playlist.')
+  const { playlist, message, queue } = params;
+  const queueConstruct = {
+    textChannel: message.channel,
+    voiceChannel: message.member.voice.channel,
+    connection: null,
+    songs: playlist.songs,
+    messages: [],
+    volume: DEFAULT_VOLUME,
+    playing: true,
+  };
+
+  queue.set(message.guild.id, queueConstruct);
+
+  logger.info('Reconnecting and resuming playlist.');
+
+  return playSong(playlist, message, queue, queueConstruct.songs[0], message.guild);
 };
