2532ed30fee93b49bb39bf2a70695acfcfc321dc
Bryan Mytko <bryanmytko@gmail.com>
2022-03-26 07:43:56 -0400

    Add reconnect ttsLead

--- src/util/tts.js
+++ src/util/tts.js
@@ -8,8 +8,7 @@ const client = new textToSpeech.TextToSpeechClient();
 const voicesPref = [26, 28, 39, 42];
 
 const ttsLead = async (message, song) => {
-  console.log('This message doesnt have a username');
-  const name = message.author.username;
+  const name = message.author.username || '';
   const leads = TTS_LEADS(song, name);
   const text = leads[Math.floor(Math.random() * leads.length)];
 
@@ -27,6 +26,23 @@ const ttsLead = async (message, song) => {
   return stream;
 };
 
+/* @TODO Refactor this / DRY it out */
+const ttsReconnectLead = async () => {
+  const request = {
+    input: { text: 'Holy shit, I just reconnected and boy are my arms tired.' },
+    voice: VOICES[voicesPref[Math.floor(Math.random() * voicesPref.length)]],
+    audioConfig: { audioEncoding: 'MP3', speakingRate: 1.0 },
+  };
+
+  const [response] = await client.synthesizeSpeech(request);
+  const stream = new Readable();
+  stream.push(response.audioContent);
+  stream.push(null);
+
+  return stream;
+}
+
 module.exports = {
   ttsLead,
+  ttsReconnectLead
 };
