22d1a8d48a749e9f36b6a5d4c075f23dcd61a9e9
Bryan Mytko <bryanmytko@gmail.com>
2022-04-08 02:51:02 -0400

    Refactor some of the ugly nesting

--- index.js
+++ index.js
@@ -25,60 +25,58 @@ mongoose.connect(`${MONGOOSE_URL}`, {
 const db = mongoose.connection;
 db.once('open', () => logger.info('Database connected.'));
 
-Playlist.findOneAndUpdate({ title: 'default' }, { title: 'default' }).then((playlist) => {
-  bot.login(process.env.DISCORD_BOT_TOKEN);
-  bot.on('ready', async () => {
-    if(!playlist) {
-      return Playlist({ title: 'default', songs: [] }).save();
-    }
+bot.login(process.env.DISCORD_BOT_TOKEN);
 
-    if(playlist.songs.length) {
-      logger.info(MSG_RECONNECTED);
+bot.on('ready', async () => {
+  const playlist = await Playlist.findOneAndUpdate({ title: 'default' }, { title: 'default' });
+  if(!playlist) return Playlist({ title: 'default', songs: [] }).save();
 
-      const channel = bot.channels.cache.get(playlist.message.channel);
-      const voice_channel = bot.channels.cache.get(playlist.message.member.voice.channel);
-      commands.reconnect({
-        playlist,
-        queue,
-        message: {
-          author: {
-            username: playlist.message.author.username,
-          },
-          channel,
-          guild: {
-            id: playlist.message.guild.id,
-          },
-          member: {
-            voice: {
-              channel: voice_channel,
-            }
-          }
-        }
-      });
-    } else {
-      logger.info(MSG_CONNECTED);
-    }
-  });
-
-  bot.on('message', async (message) => {
-    if (!validMessage(message)) return;
-
-    const match = message.content.match(commandRegex);
-    const command = match[1];
-    const params = {
-      playlist: playlist || { songs: [] },
-      queue,
-      message,
-      input: (match[2]) ? match[2].trim() : '',
-    };
-
-    try {
-      await commands[command](params);
-    } catch (e) {
-      message.channel.send(MSG_INVALID_COMMAND(command));
-      logger.error(e);
-    }
-  });
-
-  bot.on('disconnected', () => logger.fatal(MSG_DISCONNECTED));
+  if(playlist.songs) {
+    commands.reconnect(reconnectParams(playlist, bot));
+    logger.info(MSG_RECONNECTED);
+  } else {
+    logger.info(MSG_CONNECTED);
+  }
 });
+
+bot.on('message', async (message) => {
+  if (!validMessage(message)) return;
+
+  const match = message.content.match(commandRegex);
+  const command = match[1];
+  const params = {
+    queue,
+    message,
+    input: (match[2]) ? match[2].trim() : '',
+  };
+
+  commands[command] ?
+    await commands[command](params) :
+    message.channel.send(MSG_INVALID_COMMAND(command));
+});
+
+bot.on('disconnected', () => logger.fatal(MSG_DISCONNECTED));
+
+const reconnectParams = (playlist, bot) => {
+  const channel = bot.channels.cache.get(playlist.message.channel);
+  const voiceChannel = bot.channels.cache.get(playlist.message.member.voice.channel);
+
+  return {
+    playlist,
+    queue,
+    message: {
+      author: {
+        username: playlist.message.author.username,
+      },
+      channel,
+      guild: {
+        id: playlist.message.guild.id,
+      },
+      member: {
+        voice: {
+          channel: voiceChannel,
+        }
+      }
+    }
+  }
+}
