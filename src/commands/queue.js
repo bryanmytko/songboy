5c50717455b618df0a1336159c392bbd08f6c898
Bryan Mytko <bryanmytko@gmail.com>
2022-04-18 17:29:32 -0400

    Update queue command to read from db

--- src/commands/queue.js
+++ src/commands/queue.js
@@ -1,17 +1,19 @@
 const logger = require('pino')({ prettyPrint: true });
 
 const { MSG_QUEUE_EMPTY, MSG_QUEUE_CURRENT } = require('../util/messages');
+const Playlist = require('../models/playlist');
 
-module.exports = (params) => {
+module.exports = async params => {
   const { queue, message } = params;
   const serverQueue = queue.get(message.guild.id);
+  const playlist = await Playlist.findOne({ title: 'default' });
 
   try {
-    if (!serverQueue.songs.length) {
-      serverQueue.textChannel.send(MSG_QUEUE_EMPTY);
+    if (!playlist.songs) {
+      message.channel.send(MSG_QUEUE_EMPTY);
     } else {
-      const songs = serverQueue.songs.map((s, i) => `${i + 1}. ${s.title}`).join('\n');
-      serverQueue.textChannel.send(MSG_QUEUE_CURRENT(songs));
+      const songs = playlist.songs.map((s, i) => `${i + 1}. ${s.title}`).join('\n');
+      message.channel.send(MSG_QUEUE_CURRENT(songs));
     }
   } catch (e) {
     logger.error(e);
