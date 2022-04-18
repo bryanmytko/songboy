21dc3c9bda641cf0cfb7296ad848d79e91ce1632
Bryan Mytko <bryanmytko@gmail.com>
2022-04-18 18:09:08 -0400

    Improve skip for reconnect

--- src/commands/skip.js
+++ src/commands/skip.js
@@ -6,31 +6,18 @@ const Playlist = require('../models/playlist');
 module.exports = async params => {
   const { queue, message } = params;
   const playlist = await Playlist.findOne({ title: 'default' });
-  const guildId = message.guild.id || playlist.message.guild.id;
-
-  // These don't match, too tired to figure out why
-  console.log('guildId:', message.guild.id)
-  console.log('second guildId', playlist.message.guild.id)
-
-  /* Sometimes this server queue doesnt exist. Need to think about why.
-   -- works on reconnect
-   -- does not work on fresh connect ?
-   -- or vice versa now... we can use both g.id to just make sure we have a serverQueue object
-   but i do want to grok what is actually happening here */
-  const serverQueue = queue.get(guildId);
-  console.log('server q', serverQueue)
 
   if(!playlist) return;
-  if(!playlist.songs) message.channel.send(MSG_SKIP_FAIL);
+  if(!playlist.songs) return message.channel.send(MSG_SKIP_FAIL);
 
-  const skipped = playlist.songs.shift();
-  /* @TODO bug where playlist gets wiped on a single skip */
-  await playlist.save();
+  const skipped = playlist.songs[playlist.songs.length - 1];
 
+  await Playlist.findOneAndUpdate({ title: 'default' }, { $pop: { songs: 1 } }, { new: true });
+
+  /* Currently sort of working. Skips the song but still nuking the playlist */
   try {
+    for (const value of queue.values()) value.connection.dispatcher.end();
     message.channel.send(MSG_SKIP(skipped.title));
-    // @TODO this only exists on a connection where a song was queued. If we reconnect this is lost
-    serverQueue.connection.dispatcher.end();
   } catch (e) {
     logger.error(e);
   }
