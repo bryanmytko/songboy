e2d3f5eb87e6c3930dd09df518f42d7ce50146fe
Bryan Mytko <bryanmytko@gmail.com>
2022-05-08 04:15:22 -0400

    Fix empty dispatcher bug

--- src/commands/skip.js
+++ src/commands/skip.js
@@ -7,16 +7,15 @@ module.exports = async params => {
   const { queue, message } = params;
   const playlist = await Playlist.findOne({ title: 'default' });
 
-  if(!playlist) return;
-  if(!playlist.songs) return message.channel.send(MSG_SKIP_FAIL);
+  if(!playlist || !playlist.songs) return;
+  if(!playlist.songs.length) return message.channel.send(MSG_SKIP_FAIL);
 
   const skipped = playlist.songs[0];
-  const updated = await Playlist.findOneAndUpdate({ title: 'default' }, { $pop: { songs: -1 } }, { new: true });
-
-  /* Its removing first AND last song. db call returns correct so
-    the dispatcher end is removing last song somewhere */  
+  
   try {
-   /// for (const value of queue.values()) value.connection.dispatcher.end();
+    for (const value of queue.values()) {
+      value.connection && value.connection.dispatcher.end();
+    }
     message.channel.send(MSG_SKIP(skipped.title));
   } catch (e) {
     logger.error(e);
