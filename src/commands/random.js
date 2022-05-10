85e56046847cb80f7500cc2f4f5d7d42fd91fded
Bryan Mytko <bryanmytko@gmail.com>
2022-05-09 21:44:24 -0400

    Strip functionality to bare bones

--- src/commands/random.js
+++ src/commands/random.js
@@ -1,14 +1,24 @@
-const song = require('./song');
+const Player = require('../util/player');
 const Song = require('../models/song');
+const { validVoiceChannel } = require('../util/validators');  
+const { MSG_INVALID_VOICE_CHANNEL } = require('../util/messages');
 
 module.exports = async (params) => {
+  const { message } = params;
+  const textChannel = message.channel;
+
+  if (!validVoiceChannel(message)) return textChannel.send(MSG_INVALID_VOICE_CHANNEL);
+
   const count = await Song.countDocuments();
   const randomNumber = Math.floor(Math.random() * count);
   const randomResult = await Song.findOne().skip(randomNumber);
 
   if(!randomResult) return;
 
-  const newParams = { ...params, input: randomResult.url, source: 'random' };
-
-  song(newParams);
+  return Player.addSong({
+    ...params,
+    author: { username: randomResult.requester },
+    input: randomResult.url,
+    source: 'random',
+  });
 };
