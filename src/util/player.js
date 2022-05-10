124ec0bb56f162a3e2eb343dcb6dad7474c7cfd6
Bryan Mytko <bryanmytko@gmail.com>
2022-05-10 05:16:10 -0400

    WIP finish code structure for player; overwrite bug still present

--- src/util/player.js
+++ src/util/player.js
@@ -9,9 +9,7 @@ const {
   MSG_PLAYING,
   MSG_FINISHED_PLAYING,
   MSG_ADDED_TO_QUEUE,
-  MSG_INVALID_VOICE_CHANNEL,
   MSG_YOUTUBE_NOT_FOUND,
-  TTS_RANDOM,
 } = require('./messages');
 const { DEFAULT_VOLUME, YOUTUBE_WATCH_URL } = require('../util/constants');
 const Playlist = require('../models/playlist');
@@ -25,11 +23,16 @@ const entities = new Entities();
 class Player {
   constructor () {
     this.voiceChannel;
+    this.textChannel;
+    this.guildId;
+    this.connection;
   }
 
   async addSong(params) {
-    const { input, message, source, queue } = params;
+    const { input, message, source } = params;
     this.voiceChannel = message.member.voice.channel;
+    this.textChannel = message.channel;
+    this.guildId = message.guild.id;
 
     const { channel, author } = message;
     const requester = (author) ? author.username : '';
@@ -77,141 +80,85 @@ class Player {
   }
 
   async play(song) {
+    /* Check queue logic. This currently interrupts the current playing songs */
     const ttsLead = await this.getTTSLead(song);
-    const connection = await this.voiceChannel.join();
+    const audio = await ytdl(song.url, { filter: 'audioonly' });
+    const playlist = await Playlist.findOne({ title: 'default' });
 
-    return connection.play(ttsLead);
-    //   queueConstruct.connection = connection;
-    // const playSong = async (message, queue, song, guild, ttsStream) => {
+    await Playlist.findOneAndUpdate({ title: 'default' }, {
+      songs: [...playlist.songs, song],
+      message: {
+        channel: this.textChannel,
+        guild: {
+          id: this.guildId,
+        },
+        member: {
+          voice: {
+            channel: this.voiceChannel,
+          }
+        }
+      }
+    });
 
-//   if(!serverQueue.connection) {
-//     const connection = await message.member.voice.channel.join();
-//     serverQueue.connection = connection;
-//   }
+    if (!this.connection) {
+      const connection = await this.voiceChannel.join();
+      this.connection = connection;
+    }
 
-//   try {
-//     const foundSong = await ytdl(song.url, { filter: 'audioonly' });
-//     const ttsStream = await ttsLead(song);
+    if (playlist.songs.lenth) this.textChannel.send(MSG_ADDED_TO_QUEUE(song.title));
 
-//     return serverQueue
-//       .connection
-//       .play(ttsStream)
-//       .on('finish', () => {
-//         const dispatcher = serverQueue
-//           .connection
-//           .play(foundSong)
-//           .on('finish', async () => {
-//             logger.info(MSG_FINISHED_PLAYING(song.title));
-//             await saveHistory(song);
-//             const updatedPlaylist = await Playlist
-//               .findOneAndUpdate({ title: 'default' }, { $pop: { songs: -1 } }, { new: true });
-//             const newSong = updatedPlaylist.songs[0];
+    try {
+      return this.connection
+        .play(ttsLead)
+        .on('finish', () => {
+          const dispatcher = this.connection
+            .play(audio)
+            .on('finish', async () => {
+              logger.info(MSG_FINISHED_PLAYING(song.title));
 
-//             if(newSong){
-//               const ttsStream = await ttsLead(newSong);
-//               return playSong(serverQueue.messages[0], queue, newSong, guild, ttsStream);
-//             }
+              const updatedPlaylist = await Playlist
+                .findOneAndUpdate({ title: 'default' }, { $pop: { songs: -1 } }, { new: true });
+              const newSong = updatedPlaylist.songs[0];
+              await this.saveHistory(song);
 
-//             serverQueue.voiceChannel.leave();
-//             queue.delete(guild.id);
-//             return serverQueue.textChannel.send(MSG_QUEUE_EMPTY);
-//           })
-//           .on('error', async (e) => {
-//             logger.error(MSG_YOUTUBE_ERROR);
-//             logger.error('Dispatcher error: ', e);
-//             throw new Error('Dispatcher error!');
-//           });
+              if(newSong) return playSong(newSong);
 
-//         dispatcher.setVolume(DEFAULT_VOLUME);
-//         serverQueue.textChannel.send(MSG_PLAYING(song.title));
-//         return serverQueue.textChannel.send('', {
-//           files: [song.img],
-//         });
-//       });
-//   } catch (e) {
-//     logger.error(e);
-//     return serverQueue.textChannel.send(MSG_YOUTUBE_ERROR);
-//   }
-// };
+              logger.info(MSG_FINISHED_PLAYING(song.title));
+              this.voiceChannel.leave();
+              return this.textChannel.send(MSG_QUEUE_EMPTY);
+            })
+            .on('error', async (e) => {
+              logger.error(MSG_YOUTUBE_ERROR);
+              logger.error('Dispatcher error: ', e);
+              throw new Error('Dispatcher error!');
+            });
+
+          dispatcher.setVolume(DEFAULT_VOLUME);
+          this.textChannel.send(MSG_PLAYING(song.title));
+          return this.textChannel.send('', {
+            files: [song.img],
+          });
+        });
+    } catch (e) {
+      logger.error(e);
+      return this.textChannel.send(MSG_YOUTUBE_ERROR);
+    }
+  };
+
+  async saveHistory(song) {
+    const blacklistSources = ['random'];
+
+    if(blacklistSources.includes(song.source)) return;
+
+    const record = {
+      title: song.title,
+      url: song.url,
+      requester: song.requester,
+      date: new Date(),
+    };
+
+    await Song.findOneAndUpdate({ url: song.url }, record, { upsert: true });
   }
-
-
-
-
-  async saveHistory() {
-  //   const blacklistSources = ['random'];
-
-  //   if(blacklistSources.includes(song.source)) return;
-
-  //   const record = {
-  //     title: song.title,
-  //     url: song.url,
-  //     requester: song.requester,
-  //     date: new Date(),
-  //   };
-
-  //   const a = await Song.findOneAndUpdate({ url: song.url }, record, { upsert: true });
-  }
-
-}
+};
 
 module.exports = new Player();
-
-
-// const playlist = await Playlist.findOne({ title: 'default' });
-
-
-  // const voiceChannel = message.member.voice.channel;
-  // const serverQueue = queue.get(message.guild.id);
-  
-
-  // if (!serverQueue) {
-  //   const queueConstruct = {
-  //     textChannel,
-  //     voiceChannel,
-  //     connection: null,
-  //     messages: [],
-  //     volume: DEFAULT_VOLUME,
-  //     playing: true,
-  //   };
-
-  //   queue.set(message.guild.id, queueConstruct);
-  //   await Playlist.findOneAndUpdate({ title: 'default' }, {
-  //     songs: [...playlist.songs, song],
-  //     message: {
-  //       channel: message.channel,
-  //       guild: {
-  //         id: message.guild.id,
-  //       },
-  //       member: {
-  //         voice: {
-  //           channel: message.member.voice.channel,
-  //         }
-  //       }
-  //     }
-  //   });
-  //   queueConstruct.messages.push(message);
-
-  //   const connection = await voiceChannel.join();
-  //   queueConstruct.connection = connection;
-
-  //   return playSong(message, queue, song, message.guild);
-  // }
-
-  // playlist.songs.push(song);
-  // await Playlist.findOneAndUpdate({ title: 'default' }, {
-  //   songs: playlist.songs,
-  //   message: {
-  //     channel: message.channel,
-  //     guild: {
-  //       id: message.guild.id,
-  //     },
-  //     member: {
-  //       voice: {
-  //         channel: message.member.voice.channel,
-  //       }
-  //     }
-  //   }
-  // });
-  // serverQueue.messages.push(message);
-  // return message.channel.send(MSG_ADDED_TO_QUEUE(song.title));
