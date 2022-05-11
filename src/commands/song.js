3c79f59c3227362b41d07001bcaadbc0931426ea
Bryan Mytko <bryanmytko@gmail.com>
2022-05-11 03:19:25 -0400

    Expose message param

--- src/commands/song.js
+++ src/commands/song.js
@@ -3,6 +3,7 @@ const { validVoiceChannel } = require('../util/validators');
 const { MSG_INVALID_VOICE_CHANNEL } = require('../util/messages');
 
 module.exports = async params => {
+  const { message } = params;
   if (!validVoiceChannel(message)) return message.channel.send(MSG_INVALID_VOICE_CHANNEL);
 
   return Player.addSong({ ...params, source: 'request' });
