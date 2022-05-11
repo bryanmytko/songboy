bc9628bb7c8b6d4dbee11d09acaf8791670b0c45
Bryan Mytko <bryanmytko@gmail.com>
2022-05-11 03:19:50 -0400

    Begin tracing state; why does it not persist?

--- src/util/player.js
+++ src/util/player.js
@@ -25,11 +25,10 @@ class Player {
     this.voiceChannel;
     this.textChannel;
     this.guildId;
-    this.connection;
   }
 
   async addSong(params) {
-    const { input, message, source } = params;
+    const { input, message, source, state } = params;
     this.voiceChannel = message.member.voice.channel;
     this.textChannel = message.channel;
     this.guildId = message.guild.id;
@@ -53,7 +52,7 @@ class Player {
       source,
     };
 
-    this.play(songObject);
+    this.play(songObject, state);
   }
 
   async searchSong(input) {
@@ -79,11 +78,13 @@ class Player {
     }
   }
 
-  async play(song) {
-    /* Check queue logic. This currently interrupts the current playing songs */
+  async play(song, state) {
     const ttsLead = await this.getTTSLead(song);
     const audio = await ytdl(song.url, { filter: 'audioonly' });
     const playlist = await Playlist.findOne({ title: 'default' });
+    const connection = state.get('connection');
+    console.log('state', state)
+    console.log('connection', connection)
 
     await Playlist.findOneAndUpdate({ title: 'default' }, {
       songs: [...playlist.songs, song],
@@ -100,49 +101,52 @@ class Player {
       }
     });
 
-    if (!this.connection) {
+    if (!connection) {
       const connection = await this.voiceChannel.join();
-      this.connection = connection;
-    }
+      state.connection = connection;
+      console.log('new state', state)
 
-    if (playlist.songs.lenth) this.textChannel.send(MSG_ADDED_TO_QUEUE(song.title));
-
-    try {
-      return this.connection
-        .play(ttsLead)
-        .on('finish', () => {
-          const dispatcher = this.connection
-            .play(audio)
-            .on('finish', async () => {
-              logger.info(MSG_FINISHED_PLAYING(song.title));
-
-              const updatedPlaylist = await Playlist
-                .findOneAndUpdate({ title: 'default' }, { $pop: { songs: -1 } }, { new: true });
-              const newSong = updatedPlaylist.songs[0];
-              await this.saveHistory(song);
-
-              if(newSong) return playSong(newSong);
-
-              logger.info(MSG_FINISHED_PLAYING(song.title));
-              this.voiceChannel.leave();
-              return this.textChannel.send(MSG_QUEUE_EMPTY);
-            })
-            .on('error', async (e) => {
-              logger.error(MSG_YOUTUBE_ERROR);
-              logger.error('Dispatcher error: ', e);
-              throw new Error('Dispatcher error!');
+      try {
+        return connection
+          .play(ttsLead)
+          .on('finish', () => {
+            const dispatcher = connection
+              .play(audio)
+              .on('finish', async () => {
+                logger.info(MSG_FINISHED_PLAYING(song.title));
+  
+                const updatedPlaylist = await Playlist
+                  .findOneAndUpdate({ title: 'default' }, { $pop: { songs: -1 } }, { new: true });
+                const newSong = updatedPlaylist.songs[0];
+                await this.saveHistory(song);
+  
+                if(newSong) return playSong(newSong);
+  
+                logger.info(MSG_FINISHED_PLAYING(song.title));
+                this.voiceChannel.leave();
+                return this.textChannel.send(MSG_QUEUE_EMPTY);
+              })
+              .on('error', async (e) => {
+                logger.error(MSG_YOUTUBE_ERROR);
+                logger.error('Dispatcher error: ', e);
+                throw new Error('Dispatcher error!');
+              });
+  
+            dispatcher.setVolume(DEFAULT_VOLUME);
+            this.textChannel.send(MSG_PLAYING(song.title));
+            return this.textChannel.send('', {
+              files: [song.img],
             });
-
-          dispatcher.setVolume(DEFAULT_VOLUME);
-          this.textChannel.send(MSG_PLAYING(song.title));
-          return this.textChannel.send('', {
-            files: [song.img],
           });
-        });
-    } catch (e) {
-      logger.error(e);
-      return this.textChannel.send(MSG_YOUTUBE_ERROR);
+      } catch (e) {
+        logger.error(e);
+        return this.textChannel.send(MSG_YOUTUBE_ERROR);
+      }
     }
+
+    if (playlist.songs.length) this.textChannel.send(MSG_ADDED_TO_QUEUE(song.title));
+
+    
   };
 
   async saveHistory(song) {
